8b8e7f52a9f587141bbd14ff24284ac00f5bd036
joshua <Joshua.tite@me.com>
2026-02-21 20:24:12 +0000

    Add Twilio voice webhook returning TwiML

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({ extended: false }));
+
+app.post("/twilio/voice", (req, res) => {
+  res.type("text/xml");
+  res.send(`
+    <Response>
+      <Say voice="Polly.Joanna">
+        Hello. Thank you for calling. This is your AI receptionist.
+      </Say>
+    </Response>
+  `);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
